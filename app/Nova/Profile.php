3a67828110b0bddbbbe53bbde427cea38d62b5d0
REAZN <reasonwhygamer@gmail.com>
2023-02-25 18:03:50 +0000

    profiles

---
+++ app/Nova/Profile.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Profile extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\Profile>
+     */
+    public static $model = \App\Models\Profile::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'userId', 'name', 'balance', 'xp',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            Text::make('Emoji', 'emoji')
+                ->required(),
+
+            Text::make('Profile Name', 'name')
+                ->required(),
+
+            ID::make()->sortable(),
+
+            Text::make('Balance', 'balance', function () {
+                return !is_null($this->balance) ? '$' . number_format($this->balance, 0, '.', ',') : '$0';
+            })
+                ->sortable(),
+
+            Text::make('XP', 'xp', function () {
+                return !is_null($this->xp) ? number_format($this->xp, 0, '.', ',') : 0;
+            })
+                ->sortable(),
+
+            BelongsTo::make('User', 'user', User::class),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+}
