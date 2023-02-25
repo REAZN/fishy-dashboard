23e23a6470c250ab55f63819cde9dc08e1b0da85
REAZN <reasonwhygamer@gmail.com>
2023-02-25 23:09:54 +0000

    model extend

---
+++ app/Models/Model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as FishyModel;
+
+class Model extends FishyModel
+{
+    const CREATED_AT = 'createdAt';
+    const UPDATED_AT = 'updatedAt';
+}
